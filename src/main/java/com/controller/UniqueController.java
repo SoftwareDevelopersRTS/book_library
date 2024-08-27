5aea0e6533dad4dcdf16bcd70f9e7ca090624851
sanket <sanketj24.rts@gmail.com>
2024-08-27 06:22:04 +0530

    changes

--- src/main/java/com/controller/UniqueController.java
+++ src/main/java/com/controller/UniqueController.java
@@ -1,5 +1,6 @@
 package com.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,20 +10,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.bo.PaginationBO;
 import com.bo.Response;
+import com.helper.CommonMessages;
+import com.helper.ErrorConstants;
+import com.service.UserActivityService;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/api/common/")
 public class UniqueController {
+	
+	@Autowired
+	private UserActivityService userActivityService;
 
 	@PostMapping("user-activity")
 	public Response userActivity(@RequestBody PaginationBO pagination) {
 		Response response = new Response();
 		try {
-			
+			return userActivityService.userActivity(pagination);
 		}
 		catch(Exception e) {
-			
+			response.setStatus(ErrorConstants.SUCESS);
+			response.setMessage(CommonMessages.SOMETHING_WENT_WRONG_TRY_AGAIN);			
 		}
 		return response;
 	}
