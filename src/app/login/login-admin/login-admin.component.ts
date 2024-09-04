bbbfd5a390b29162cd0ee50767f8e01d30d7bbea
Sanket Jain <sanketj24.rts@gmail.com>
2024-09-04 22:38:04 +0530

    add ngselect on login page

--- src/app/login/login-admin/login-admin.component.ts
+++ src/app/login/login-admin/login-admin.component.ts
@@ -1,16 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { NgSelectModule } from '@ng-select/ng-select';
 @Component({
   selector: 'app-login-admin',
   standalone: true,
-  imports: [MatIconModule,CommonModule],
+  imports: [MatIconModule,CommonModule,NgSelectModule,FormsModule],
   templateUrl: './login-admin.component.html',
   styleUrl: './login-admin.component.css'
 })
 export class LoginAdminComponent {
   isPasswordVisible: boolean = false;
+  adminData:any={};
+  LibraryRoles = [
+    { id: 1, name: 'Admin' },
+    { id: 2, name: 'Librarian' },
+    { id: 3, name: 'Book Inventory Manager' },
+  ];
   togglePassword() {
     this.isPasswordVisible = !this.isPasswordVisible;
   }
+
 }
