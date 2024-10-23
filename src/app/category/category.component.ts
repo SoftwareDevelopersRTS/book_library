39ff2697ebef00a78686a20d85913c960e2730ef
sanket <sanketj24.rts@gmail.com>
2024-10-23 22:02:55 +0530

    Change for edit category

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -53,10 +53,10 @@ export class CategoryComponent implements OnInit {
 
   async onUpload(event: Event, fileType: string) {
     const input = event.target as HTMLInputElement; // Cast the event target to HTMLInputElement
-  
+
     if (input.files && input.files.length > 0) {
       const file = input.files[0]; // Get the first file
-  
+
       try {
         // Use a switch statement for cleaner control flow over fileType
         switch (fileType) {
@@ -64,44 +64,72 @@ export class CategoryComponent implements OnInit {
             this.imageDataBo.encodedFrontImage = await this.fileUtil.base64Provider(file);
             console.log('Frontside Image uploaded:', file);
             break;
-  
+
           case 'backImage':
             this.imageDataBo.encodedBackImage = await this.fileUtil.base64Provider(file);
             console.log('Backside Image uploaded:', file);
             break;
-  
+
           case 'bookFile':
             // Handle book file if necessary
             console.log('Book file uploaded:', file);
             break;
-  
+
           default:
             console.error('Unknown fileType:', fileType);
             break;
         }
-  
+
         // Log the uploaded image data
         console.log("Encoded Front Image:", this.imageDataBo.encodedFrontImage);
         console.log("Encoded Back Image:", this.imageDataBo.encodedBackImage);
-  
+
       } catch (error) {
         console.error('Error during file upload:', error);
       }
-  
+
     } else {
       console.error('No file selected');
     }
   }
-  
 
-  addCategory() {
-    this.category.imageDataBo = this.imageDataBo;
-    this.common.postRequest(this.common.SERVER_URL['ADD_BOOK_CATEGORY'], this.category).subscribe(
-      (response) => {
 
+  addEditCategory() {
+    if (this.category.bookCategoryId && this.category.bookCategoryUniqueId) {
+
+    } else {
+      this.category.imageDataBo = this.imageDataBo;
+      this.common.postRequest(this.common.SERVER_URL['ADD_BOOK_CATEGORY'], this.category).subscribe(
+        (response) => {
+
+        }
+      );
+    }
+
+  }
+
+  changeBookCategoryStatus(bookCategoryId: number) {
+    this.common.getRequest(this.common.SERVER_URL['CHANGE_BOOK_CATEGORY_STATUS'] + bookCategoryId).subscribe(
+      (response: any) => {
+        if (response.status == STATUS_CODES.SUCCESS) {
+          this.bookCategoryList()
+        }
       }
-    );
+    )
 
+  }
+  editBookCategory(bookCategory: any) {
+    this.category = bookCategory;
+    this.openCloseModal('show')
+  }
+
+
+  getCategoryById(bookCategoryId: number) {
+    this.common.getRequest(this.common.SERVER_URL['GET_BOOK_CATEGORY_BY_ID']).subscribe((response: any) => {
+      if (response.status == 200) {
+        this.category = response.result;
+      }
+    })
   }
 
 
