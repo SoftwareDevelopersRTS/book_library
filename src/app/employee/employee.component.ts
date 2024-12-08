4795315541fed46dd9acca1f5eeaada0c971006c
sanket <sanketj24.rts@gmail.com>
2024-12-08 10:21:30 +0530

    shown employee list properly

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { CommonService } from '../common.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-employee',
   standalone: true,
-  imports: [SidebarComponent],
+  imports: [SidebarComponent, FormsModule, CommonModule, NgxPaginationModule],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
 export class EmployeeComponent implements OnInit {
 
-  employeeListPagination: any = {};
+  employeeListPagination: any = { pageNo: 1, numPerPage: 10 };
+  employeeList: any = [];
+  employeeCount: number = 0;
   constructor(private common: CommonService) {
 
   }
@@ -21,10 +26,18 @@ export class EmployeeComponent implements OnInit {
 
 
   getEmployeeList() {
-    this.common.postRequest(this.common.SERVER_URL['EMPLOYEE_LIST'],this.employeeListPagination).subscribe((response) => {
+    this.common.postRequest(this.common.SERVER_URL['EMPLOYEE_LIST'], this.employeeListPagination).subscribe((response) => {
       if (response.status == 200) {
+        this.employeeList = response.result;
+        this.employeeCount = response.listCount;
+      } else {
 
       }
     })
+  }
+
+  changePage(pageNo: any) {
+    this.employeeListPagination.pageNo = pageNo;
+    this.getEmployeeList();
   }
 }
