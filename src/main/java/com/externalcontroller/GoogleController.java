4c4f1ca9587e7e57ee04632849564a8c929315a1
sanket <sanketj24.rts@gmail.com>
2024-12-12 19:22:02 +0530

    small change

--- src/main/java/com/externalcontroller/GoogleController.java
+++ src/main/java/com/externalcontroller/GoogleController.java
@@ -1,15 +1,33 @@
 package com.externalcontroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bo.ImageDataBO;
+import com.bo.Response;
+import com.utils.FileUtility;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/api/google-drive/")
 public class GoogleController {
-	
-	
-	
+
+	@Autowired
+	private FileUtility fileUtility;
+
+	@PostMapping("upload-base64-test")
+	public Response uploadBase64Test(@RequestBody ImageDataBO imageData) {
+		Response response = new Response();
+		try {
+
+		} catch (Exception e) {
+
+		}
+		return response;
+	}
 
 }
